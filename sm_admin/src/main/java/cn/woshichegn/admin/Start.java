088181b4ac25cf2a4b77254301d05b59b4c186ee
chenjianfan <316031139@qq.com>
2018-11-14 18:28:58 +0800

    代码

--- sm_admin/src/main/java/cn/woshichegn/admin/Start.java
+++ sm_admin/src/main/java/cn/woshichegn/admin/Start.java
@@ -1,13 +1,22 @@
 package cn.woshichegn.admin;
+
 import org.apache.log4j.Logger;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
 
 import cn.woshicheng.common.util.SystemIpUtil;
+
+/**
+ * Java -Xms2048m -Xmx2048m -XX:MaxPermSize=128m -jar sm_admin.war
+ * 
+ * @author Administrator
+ *
+ */
+
 public class Start {
-	static Logger logger = Logger.getLogger("Start");
 
 	public static void main(String[] args) {
+		Logger logger = Logger.getLogger("Start");
 
 		int port = 8081;
 		Server server = new Server(port);
@@ -28,4 +37,5 @@ public class Start {
 		}
 
 	}
+
 }
